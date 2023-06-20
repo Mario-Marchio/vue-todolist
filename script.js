ec314a3288943bcc63d19f46e01400e9fc5892c7
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-20 22:05:42 +0200

    added list to html

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ console.log(`vue ok`);
 const { createApp } = Vue;
 
 const app = createApp({
-    data: {
-        tasks: [
-            {text: `comprare il latte`, done:false, },
-            {text: `comprare il pane`, done:false,},
-            {text: `comprare il miele`, done:false, },
-            {text: `comprare il cocco`, done:false, }
-        ],
-    },
+    data() {
+        return{
+            tasks: [
+                {text: `comprare il latte`, done: false, },
+                {text: `comprare il pane`, done: false,},
+                {text: `comprare il miele`, done: false, },
+                {text: `comprare il cocco`, done:false, }
+            ],
+        }
+    }
  });
 
 app.mount(`#root`);
